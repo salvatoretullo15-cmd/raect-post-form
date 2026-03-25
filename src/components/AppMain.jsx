842d5fe78abd5835a75fefd8c5b5366d969c4f44
salvatore tullo <salvatoretullo15@gmail.com>
2026-03-25 01:21:33 +0100

    implemento delle funzioni handle submit e handle on change con chiamata api usando axios

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import axios from 'axios';
 
 export default function AppMain() {
 
@@ -12,7 +12,20 @@ export default function AppMain() {
 
     const api_url = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts"
 
-    
+    function handleOnChange(e) {
+        const { name, value, type, checked } = e.target;
+        setFormData({...formData,[name]: type === "checkbox" ? checked : value});
+    }
+
+    function handleSubmit(e) {
+    e.preventDefault(api_url, formData);
+    axios.post("https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts", formData)
+        .then(res => {console.log("post creato con successo",res.data)
+    })
+        .catch(err => {
+            console.error("Errore 404 o altro:", err);
+        });
+    }
     return(
         <main className='container'>
             
