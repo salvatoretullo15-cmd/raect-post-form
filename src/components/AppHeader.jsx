3fb9a6242cd68fd38fc9fa6ef87bf24553ad2519
salvatore tullo <salvatoretullo15@gmail.com>
2026-03-25 01:18:58 +0100

    Add Bootstrap, Axios, and Popper.js dependencies; update AppHeader and AppMain components

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,6 +1,7 @@
 export default function AppHeader() {
     return(
-        <>
-        </>
+        <header className="container">
+            <h1>FORM DI INVIO DATI</h1>
+        </header>
     )
 }
--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,6 +1,21 @@
+import { useState } from 'react'
+
+
 export default function AppMain() {
+
+    const [formData, setFormData] = useState({
+        author: "",
+        title: "",
+        body: "",
+        public: false 
+    })
+
+    const api_url = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts"
+
+    
     return(
-        <>
-        </>
+        <main className='container'>
+            
+        </main>
     )
 }
